1ddac0e5f5067b5033245b98e61356b27e8b13ae
mizu <mizucodes@gmail.com>
2024-04-19 02:14:00 -0400

    re-work, init for stream gather, will get hls

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from openai import OpenAI
+import streamlink
 
-
-client = OpenAI
-
+streams = streamlink.streams("https://twitch.tv/xqc")
+if 'best' in streams:
+    stream_url = streams['best'].url
+    print("Best available stream URL:", stream_url)
+    
+else:
+    print("No available streams found.")
+s
